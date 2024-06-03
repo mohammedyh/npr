d55c03c710bd0acdbb000a0bf4a2a79b399875cd
mohammedyh <mohammedyousufhamid@gmail.com>
2024-06-03 21:01:18 +0100

    feat: add bubbletea to render scripts in a list

--- main.go
+++ main.go
@@ -4,11 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type Script struct {
-	key   string
-	value string
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type script struct {
+	title, description string
+}
+
+func (s script) Title() string       { return s.title }
+func (s script) Description() string { return s.description }
+func (s script) FilterValue() string { return s.title }
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
 }
 
 func main() {
@@ -27,12 +64,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	result, err := json.MarshalIndent(parsedJson["scripts"], "", " ")
+	var items []list.Item
 
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	for scriptKey, scriptValue := range parsedJson["scripts"].(map[string]interface{}) {
+		fmt.Printf("%v: %v\n", scriptKey, scriptValue)
+
+		items = append(items, script{title: scriptKey, description: scriptValue.(string)})
 	}
 
-	fmt.Println(string(result))
+	fmt.Println(items)
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "Scripts to Run"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
