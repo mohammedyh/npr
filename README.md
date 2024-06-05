9f90961c3c4c67264cd3245112c491de75b2d951
mohammedyh <mohammedyousufhamid@gmail.com>
2024-06-05 02:38:28 +0100

    feat: show message for multiple present lockfiles

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ A CLI tool that lets you browse and execute npm, pnpm, yarn, or bun scripts dire
 - [x] Check for `package.json` in current directory / project
 - [x] Read file and parse key and values in `scripts` object in `package.json`
 - [x] Print scripts to stdout as a list - use BubbleTea
-- [ ] Detect the package manager - using the lockfile?
+- [x] Detect the package manager - using the lockfile?
 - [x] When enter is pressed for a chosen script, that script is run
 
 ### Nice-To-Have Features
 - [ ] Install dependencies if `node_modules` isn't found
+- [ ] Print something if multiple lockfiles are found
--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
+var docStyle = lipgloss.NewStyle().Margin(1, 2).Foreground(lipgloss.Color("222"))
 
 type Script struct {
 	name, command string
@@ -23,8 +24,57 @@ func (s Script) Title() string       { return s.name }
 func (s Script) Description() string { return s.command }
 func (s Script) FilterValue() string { return s.name }
 
-func runScript(scriptName string) tea.Cmd {
-	command := exec.Command("npm", "run", scriptName)
+func detectPackageManager() string {
+	var packageManager string
+	var lockfiles []string
+	cwd, cwdErr := os.Getwd()
+
+	if cwdErr != nil {
+		fmt.Println(docStyle.Margin(0, 2).Render("Unable to get current directory"))
+		os.Exit(1)
+	}
+
+	files, readDirErr := os.ReadDir(cwd)
+
+	if readDirErr != nil {
+		fmt.Println(docStyle.Margin(0, 2).Render("Unable to read files and folders current directory"))
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		if strings.Contains(file.Name(), "lock") {
+			lockfiles = append(lockfiles, file.Name())
+		}
+
+		if len(lockfiles) > 1 {
+			fmt.Println(docStyle.Margin(0, 2).Render("Muliple lockfiles found in", cwd))
+			os.Exit(1)
+			break
+		}
+
+		if !file.IsDir() {
+			switch file.Name() {
+			case "pnpm-lock.yaml":
+				packageManager = "pnpm"
+			case "package-lock.json":
+				packageManager = "npm"
+			case "bun.lockb":
+				packageManager = "bun"
+			case "yarn.lock":
+				packageManager = "yarn"
+			}
+		}
+	}
+
+	if packageManager == "" {
+		packageManager = "npm"
+	}
+
+	return packageManager
+}
+
+func runScript(packageManager, scriptName string) tea.Cmd {
+	command := exec.Command(packageManager, "run", scriptName)
 	return tea.ExecProcess(command, func(err error) tea.Msg {
 		if err != nil {
 			return tea.Quit()
@@ -38,6 +88,7 @@ type model struct {
 }
 
 func (m model) Init() tea.Cmd {
+	detectPackageManager()
 	return tea.SetWindowTitle("np-run")
 }
 
@@ -50,7 +101,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		if msg.String() == "enter" {
 			script, _ := m.list.SelectedItem().(Script)
-			return m, runScript(script.name)
+			return m, runScript(detectPackageManager(), script.name)
 		}
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
